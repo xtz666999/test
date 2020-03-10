ba35b0601cb74c93d7bc1003ced20aa2723c1031
Yu-hu-hu! <qwqw@qwqw.ru>
2020-03-10 10:54:58 +0300

    третий коммит

--- js/main.js
+++ js/main.js
@@ -1,31 +1,68 @@
 window.onload = function() {
 
-    'use strict';
+    // 'use strict';
     
-    let dude = {
-        name: 'xxx',
-        age: 30,
-        isDumbass: true,
+    // let dude = {
+    //     name: 'xxx',
+    //     age: 30,
+    //     isDumbass: true,
+    // };
+    
+    // console.log(dude.name);
+
+    // let arr = ['xxx.png', 'yyy.png', 'sss.jpeg'];
+
+    // console.log(arr[1]); 
+
+    // let div = document.querySelector('.new');
+    // let click = document.querySelector('.click');
+
+
+    // click.addEventListener('mouseover', function() {
+    //     div.innerHTML = 'XXX';
+    // });
+
+    // click.addEventListener('mouseout', function() {
+    //     div.innerHTML = 'YYY';
+    // });
+    
+  
+
+    // let num = 50;
+
+    // if (num < 49) {
+    //     alert('No!');
+    // } else if (num > 100) {
+    //     alert('Too mach');
+    // } else {
+    //     alert('YEEEEEEEEE');
+    // }
+
+    // (num < 55) ? alert('No!') : alert('YEEEEEEEEE');
+
+    let money = +prompt('Month budget?');
+    let time = +prompt('Enter date YYYY-MM-DD');
+    let value;
+
+    let appData = {
+        budget: money,
+        timeData: time,
+        expenses: {
+            key: value,
+        },
+        optionalExpenses: {},
+        income: [],
+        saving: false,
     };
-    
-    console.log(dude.name);
 
-    let arr = ['xxx.png', 'yyy.png', 'sss.jpeg'];
+    let shoping = prompt('Expenses');
+    let howMach = +prompt('How much?');
 
-    console.log(arr[1]); 
+    appData.expenses.key = shoping;
+    appData.expenses.value = howMach;
 
-    let div = document.querySelector('.new');
-    let click = document.querySelector('.click');
-
-
-    click.addEventListener('mouseover', function() {
-        div.innerHTML = 'XXX';
-    });
-
-    click.addEventListener('mouseout', function() {
-        div.innerHTML = 'YYY';
-    });   
-
+    alert(appData.expenses.key);
+    alert(appData.expenses.value);
 
 };
 
